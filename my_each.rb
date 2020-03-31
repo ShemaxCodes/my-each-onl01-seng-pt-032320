53e9548d3851e05fd2d90bec3c70b74783dd14ae
Jashema Panapa <jcpanapa@gmail.com>
2020-03-31 17:54:52 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,8 @@
-def my_each(array) # put argument(s) here
- collection = []
- 
- i = 0 
- 
-  while i < collection.length
-  yield collection[i]
-  
- i = i + 1 
+def my_each(array)
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i += 1
   end
- collection
-
+  array
 end
