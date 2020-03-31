144a4fd807f6a28642c7e188cfc2a1d8f9c3de77
Jashema Panapa <jcpanapa@gmail.com>
2020-03-31 17:49:11 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,10 @@
 def my_each(array) # put argument(s) here
-if block_given?
  collection = []
  
  i = 0 
  
   while i < collection.length
-  yield (collection[i])
+  yield collection[i]
   
  i = i + 1 
   end
